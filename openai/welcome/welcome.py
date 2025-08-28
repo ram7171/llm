aec6c5870485dda57000b79e591b267809f4c7dd
ramanathan anandhan <ramanathan.anandh@gmail.com>
2025-08-28 11:11:05 +0530

    readme update

--- openai/welcome/welcome.py
+++ openai/welcome/welcome.py
@@ -1,4 +1,14 @@
+import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 print("hello world")
 
+def main():
+    print("This is the main function.")
+    print(os.environ.get("OPENAI_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
